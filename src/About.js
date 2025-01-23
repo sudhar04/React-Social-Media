8b8f076a68b04daf54f538c592f614b044f6d38e
sudhar04 <sudharsananr04@gmail.com>
2025-01-23 10:43:18 +0530

    first commit

---
+++ src/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+      <main className='About'>
+          <h2>About</h2>
+          <p style={{ marginTop: "1rem" }}>Hi, This is Sudharsanan : RS Social Media</p>
+      </main>
+  )
+}
+
+export default About
